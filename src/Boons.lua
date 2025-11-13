d314dd167f6431646ab3401c80dd67ff29444170
Fayde15 <dragogamer6565@gmail.com>
2025-11-13 16:06:37 -0500

    Added TycheAttackBoon using Aphrodite template

--- src/Boons.lua
+++ src/Boons.lua
@@ -1,42 +1,80 @@
 -- Creating the boon functions itself
---game.TraitData.Tyche_Boon = {
-    --InheritFrom = { "BaseTrait", "EarthBoon" },
-    --Elements = { "Earth" }, -- Need to add this even if you inherit
-    --Name = "Tyche_Attack_Boon",
-    --BoonInfoTitle = "Tyche_Attack_",
-   -- Icon = "Boon_Tyche_TycheWeaponBoon",
-   -- Slot = "Melee",
-   -- TraitOrderingValueCache = 50,
-  --  Cost = 30,
-  --  BlockStacking = false,
-  --  RarityLevels = {
-  --      Common = {
-   --         Multiplier = 1.00,
-   --     },
-   --     Rare = {
-   --         Multiplier = 1.50,
-   --     },
-   --     Epic = {
-   --         Multiplier = 2.00,
-    --    },
-   --     Heroic = {
-   --         Multiplier = 2.50,
-   --     },
+TycheAttackBoon = game.TraitData.TycheAttackBoon,
+game.TraitData.TycheAttackBoon = {
+    InheritFrom = { "BaseTrait", "EarthBoon" },
+    Elements = { "Earth" }, -- Need to add this even if you inherit,
+    Name = "Tyche_Attack_Boon",
+    BoonInfoTitle = "Tyche_Attack_",
+    Icon = "Boon_Aphrodite_27",
+    Slot = "Melee",
+    TraitOrderingValueCache = 50,
+  Cost = 30,
+   BlockStacking = false,
+  RarityLevels = {
+     Common = {
+            Multiplier = 1.00,
+      },
+       Rare = {
+         Multiplier = 1.50,
+      },
+       Epic = {
+           Multiplier = 2.00,
+       },
+      Heroic = {
+           Multiplier = 2.50,
+       },
+		AddOutgoingDamageModifiers =
+		{
+			ValidWeapons = WeaponSets.HeroPrimaryWeapons,
+			ConditionalValidWeapon = 
+			{
+				TraitName = "TransformPrimaryTalent",
+				WeaponName = "WeaponTransformAttack"
+			},
+			ProximityThreshold = 430,
+			ProximityThresholdExclusionBoon = "AllCloseBoon",
+			ProximityMultiplier = 
+			{ 
+				BaseValue = 1.8,
+				SourceIsMultiplier = true,
+
+				AbsoluteStackValues =
+				{
+					[1] = 1.25,
+					[2] = 1.15,
+					[3] = 1.10,
+				},
+			},
+			ReportValues = 
+			{ 
+				ReportedWeaponMultiplier = "ProximityMultiplier",
+			},
+		},
+		StatLines =
+		{
+			"NearbyDamageStatDisplay1",
+		},
+		ExtractValues =
+		{
+			{
+					Key = "ReportedWeaponMultiplier",
+					ExtractAs = "TooltipDamageBonus",
+					Format = "PercentDelta",
+			},
+		},
+		WeaponDataOverride = 
+		{
+			WeaponSuitCharged = 
+			{
+				BlockGraphic = "SuitDeflect_Aphrodite",
+			}
+		},
+	},
 
 local TycheTraitDictionary = {
-	SupportingFireBoon = true,
-	CritBonusBoon = true,
-	DashOmegaBuffBoon = true,
-	HighHealthCritBoon = true,
-	InsideCastCritBoon = true,
-	OmegaCastVolleyBoon = true,
-	TimedCritVulnerabilityBoon = true,
-	-- New Boons
-	ArtemisWeaponBoon = true,
-	ArtemisSpecialBoon = true,
-	ArtemisCriticalBoon = true,
-	ArtemisArmourBoon = true,
-}
+	TycheAttackBoon = true,
+},
 
-game.ScreenData.BoonInfo.TraitDictionary.TycheUpgrade = ArtemisTraitDictionary
-game.LootData.TycheUpgrade.TraitIndex = TycheTraitDictionary
+game.ScreenData.BoonInfo.TraitDictionary.TycheUpgrade = TycheTraitDictionary,
+game.LootData.TycheUpgrade.TraitIndex = TycheTraitDictionary,
+game.TratData.TycheUpgrade = TycheTraitDictionary,
